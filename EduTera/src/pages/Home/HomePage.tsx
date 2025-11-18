8a06768dea41ca902d28c44f61b0351895e972f9
asmaaelbahrawi1 <asmaaelbahrawi@std.mans.edu.eg>
2025-11-18 21:26:31 +0200

    setup project structure + styles + theme

---
+++ EduTera/src/pages/Home/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function HomePage() {
+  return <div style={{
+      background: "var(--color-primary)",
+      color: "white",
+      padding: "20px",
+      fontFamily: "var(--font-main)"
+    }}>
+      Theme is working! 🎉
+    </div>
+}
