5e3272835b94f8a4cddc119076aa132501d33da7
malakeltalkhawy <133808072+malakeltalkhawy@users.noreply.github.com>
2025-11-20 16:09:48 +0200

    NavBar

---
+++ EduTera/src/components/layout/Navbar/NavBar.tsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+import Logo from '../../../assets/Images/logo.png'
+import { Link } from 'react-router-dom'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav-left">
+        <img src={Logo}  alt="EduTera Logo" className="logo-image" />
+        <div className="logo-text">
+          <h1 className="title">Edu<span style={{color:"#68AAB6"}}>T</span>era</h1>
+          <span className="subtitle">Smart Learning. Bright Minds.</span>
+        </div>
+      </div>
+
+      <div className="nav-links">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Our Product</a>
+      </div>
+
+    <div className="nav-buttons">
+        <Link to="/demo">
+            <button className="btn-demo">Request a demo</button>
+        </Link>
+        <Link to="/signin">
+            <button className="btn-signin">Sign in</button>
+        </Link>
+    </div>
+    </nav>
+  )
+}
+
+export default NavBar
