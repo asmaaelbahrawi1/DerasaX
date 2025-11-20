1eba9ee918c85c6e4826e55196922ee702eea9a1
malakeltalkhawy <133808072+malakeltalkhawy@users.noreply.github.com>
2025-11-20 17:04:05 +0200

    edit navbar

--- EduTera/src/pages/Home/HomePage.tsx
+++ EduTera/src/pages/Home/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Footer from '../../components/layout/Footer/Footer'
+import NavBar from '../../components/layout/Navbar/NavBar'
+
 
 export default function HomePage() {
   return <div>
+            <NavBar />
              <Footer />
+
          </div>
     
 }
