213787cde0237e61667db56547edb7574e682ad8
Hazem-Ahmed <hazemahm@std.mans.edu.eg>
2025-11-23 18:07:58 +0200

    create hero section

--- DerasaX/src/pages/Home/HomePage.tsx
+++ DerasaX/src/pages/Home/HomePage.tsx
@@ -3,7 +3,7 @@ import Footer from '../../components/layout/Footer/Footer'
 import NavBar from '../../components/layout/Navbar/NavBar'
 import FaqButton from "../../components/FAQ/FAQButton"
 
-
+import HeroSection from './HomePageSections/HeroSection/HeroSection'
 
 
 
@@ -11,6 +11,7 @@ import FaqButton from "../../components/FAQ/FAQButton"
 export default function HomePage() {
   return <div>
             <NavBar />
+            <HeroSection />
             <FaqButton />
             
              <Footer />
