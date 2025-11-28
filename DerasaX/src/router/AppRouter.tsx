1f9dfd7dc31e2ed6c60c2fa10c8da9e9ef220dee
asmaaelbahrawi1 <asmaaelbahrawi@std.mans.edu.eg>
2025-11-28 06:09:28 +0200

    ceate request a demo page

--- DerasaX/src/router/AppRouter.tsx
+++ DerasaX/src/router/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/Home/HomePage";
+import RequestDemoPage from "../pages/RequestDemo/RequestDemoPage"
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/RequestDemoPage" element={<RequestDemoPage />} />
     </Routes>
   );
 }
