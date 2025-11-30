d62d8262f18c935643fa6fc7f72f07ffc0e11840
asmaaelbahrawi1 <asmaaelbahrawi@std.mans.edu.eg>
2025-11-30 21:24:48 +0200

    finishing tabbed navigation pages with activity , events , news pages

--- DerasaX/src/pages/Activities/Activities.tsx
+++ DerasaX/src/pages/Activities/Activities.tsx
@@ -3,12 +3,13 @@ import '../../styles/global.css'
 import '../../styles/theme.css'
 import NavBar from '../../components/layout/Navbar/NavBar'
 import Footer from '../../components/layout/Footer/Footer'
+import TabbedNavigation from '../../components/UI/TabbedNavigation'
 
 export default function Activities(){
     return(
         <>
             <NavBar/>
-
+            <TabbedNavigation />
             <Footer/>
         </>
 
